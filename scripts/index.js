35c8285429574193e448143874a268e79633e54a
Алексей Алексеев <alexseev617@gmail.com>
2024-10-25 14:40:48 +0300

    Добавил комментарии для понимания

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,9 @@
+//скролл кнопки к каталогу
 document.getElementById('main-action-buttom').onclick = function () {
     document.getElementById('products').scrollIntoView({ behavior: 'smooth' })
 };
 
+//Скролл меню навигации к секциям
 const links = document.querySelectorAll('.menu-item > a');
 for (let i = 0; i < links.length; i++) {
     links[i].onclick = function () {
@@ -9,6 +11,7 @@ for (let i = 0; i < links.length; i++) {
     };
 }
 
+//Скролл кнопок карточек к секции заказ
 const button = document.querySelectorAll('.products-item .button');
 for (let i = 0; i < button.length; i++) {
     button[i].onclick = function () {
@@ -16,7 +19,7 @@ for (let i = 0; i < button.length; i++) {
     };
 }
 
-
+//Смена валюты
 const prices = document.getElementsByClassName('products-item-price');
 document.getElementById('change-currency').onclick = function (e) {
     const currentCurrency = e.target.innerText;
@@ -43,7 +46,7 @@ document.getElementById('change-currency').onclick = function (e) {
     }
 }
 
-
+//Валидация
 const product = document.getElementById('product');
 const name = document.getElementById('name');
 const phone = document.getElementById('phone');
